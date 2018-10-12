d69f00158a6302ab521e360d497dd38dc88236ad
Sean Pheneger <wcolts2000@gmail.com>
2018-10-11 22:55:15 -0700

    apex charts api for nice visualization charts of data

---
+++ apexchart/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import PopChart from './components/popChart';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <PopChart />
+      </div>
+    );
+  }
+}
+
+export default App;
